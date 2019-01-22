4815f63395b87c3d5bdb645ed543ffad7e0dfa21
Mahmoud Adas <mido3ds@gmail.com>
2019-01-22 20:25:19 +0200

    added loggers

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,49 @@
 #include <cstdio>
 #include <functional>
 #include <cstdint>
+#include <string>
+#include <cstdarg>
 using namespace std;
 
+#include "Date.h"
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+void logInfo(string format, ...) {
+    format = string(ANSI_COLOR_BLUE "[INFO]" ANSI_COLOR_RESET) + string(GMTDateTime()) + ": " + format + '\n';
+
+    va_list args;
+    va_start (args, format);
+    vfprintf (stdout, format.c_str(), args);
+    va_end (args);
+}
+
+void logWarning(string format, ...) {
+    format = string(ANSI_COLOR_MAGENTA "[WARNING]" ANSI_COLOR_RESET) + string(GMTDateTime()) + ": " + format + '\n';
+
+    va_list args;
+    va_start (args, format);
+    vfprintf (stdout, format.c_str(), args);
+    va_end (args);
+}
+
+void logError(string format, ...) {
+    format = string(ANSI_COLOR_RED "[ERROR]" ANSI_COLOR_RESET) + string(GMTDateTime()) + ": " + format + '\n';
+
+    va_list args;
+    va_start (args, format);
+    vfprintf (stderr, format.c_str(), args);
+    va_end (args);
+}
+
 struct Registers {
-    uint16_t pc; // program coutner
+    uint16_t pc; // program counter
     uint8_t sp; // stack pointer
     uint8_t a; // accumulator
     uint8_t x; // index
