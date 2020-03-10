c47b8606a40d8017b1748b6272ecb4d55785be91
Mahmoud Adas <mido3ds@gmail.com>
2020-03-10 15:09:22 +0200

    log prints line number with file name

--- src/log.cpp
+++ src/log.cpp
@@ -5,29 +5,35 @@
 #define YELLOW  "\033[0;33m"
 #define RESET   "\033[0m"
 
+string _path;
+
+void setPath(string path, size_t size, int line) {
+    _path = path.substr(size, path.size()-size).c_str() + string(":") + std::to_string(line);
+}
+
 void logInfo(string format, ...) {
-    format = GREEN"[INFO] "RESET + format + '\n';
+    format = GREEN"[INFO " + _path + RESET + "] " + format + '\n';
 
     va_list args;
-    va_start (args, format);
-    vfprintf (stdout, format.c_str(), args);
-    va_end (args);
+    va_start(args, format);
+    vfprintf(stdout, format.c_str(), args);
+    va_end(args);
 }
 
 void logWarning(string format, ...) {
-    format = YELLOW"[WARNING] " + format + RESET"\n";
+    format = YELLOW"[WARNING " + _path + "] " + format + RESET"\n";
 
     va_list args;
-    va_start (args, format);
-    vfprintf (stdout, format.c_str(), args);
-    va_end (args);
+    va_start(args, format);
+    vfprintf(stdout, format.c_str(), args);
+    va_end(args);
 }
 
 void logError(string format, ...) {
-    format = RED"[ERROR] " + format + RESET"\n";
+    format = RED"[ERROR " + _path + "] "+ format + RESET"\n";
 
     va_list args;
-    va_start (args, format);
-    vfprintf (stderr, format.c_str(), args);
-    va_end (args);
+    va_start(args, format);
+    vfprintf(stderr, format.c_str(), args);
+    va_end(args);
 }
