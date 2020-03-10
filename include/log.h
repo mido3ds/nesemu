4ed3ac4240cec560fe8b98d4bc3bedb747b41518
Mahmoud Adas <mido3ds@gmail.com>
2020-03-10 15:09:22 +0200

    log prints line number with file name

--- include/log.h
+++ include/log.h
@@ -8,19 +8,24 @@
 
 using std::string;
 
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+#define __FILENAME__ __FILE__ + SOURCE_PATH_SIZE
+void setPath(string path, size_t size, int line);
+
 #ifndef TEST
 void logInfo(string format, ...);
-#define INFO logInfo
+#define INFO(...) setPath(__FILE__, SOURCE_PATH_SIZE, __LINE__); logInfo(__VA_ARGS__)
 #else
 #define INFO(...)
 #endif
 
 #ifndef IGNORE_WARNINGS
 void logWarning(string format, ...);
-#define WARNING logWarning
+#define WARNING(...) setPath(__FILE__, SOURCE_PATH_SIZE, __LINE__); logWarning(__VA_ARGS__)
 #else
 #define WARNING(...)
 #endif
 
 void logError(string format, ...);
-#define ERROR logError
+#define ERROR(...) setPath(__FILE__, SOURCE_PATH_SIZE, __LINE__); logError(__VA_ARGS__)
